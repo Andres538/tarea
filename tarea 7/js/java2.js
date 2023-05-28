f509b2b008f1bc50f8fdc681b579753ec02a72fb
Andres538 <an.ribet98@gmail.com>
2023-05-28 13:19:02 -0300

    Tarea 7 incompleta

---
+++ tarea 7/js/java2.js
@@ -0,0 +1,63 @@
+var alumnos = [
+
+  {
+
+    nombre: 'Juan Gomez',
+
+    nota: 7
+
+  }, {
+
+    nombre: 'Pedro Rodriguez',
+
+    nota: 4
+
+  }, {
+
+    nombre: 'Roxana García',
+
+    nota: 8
+
+  }, {
+
+    nombre: 'Luciano Lopez',
+
+    nota: 5
+
+  }, {
+
+    nombre: 'Fernanda Gimenez',
+
+    nota: 6
+
+  }, {
+
+    nombre: 'Florencia Martinez',
+
+    nota: 10
+
+  }, {
+
+    nombre: 'Raul Sanchez',
+
+    nota: 7
+
+  }, {
+
+    nombre: 'Sandra Figueroa',
+
+    nota: 8
+
+  }
+
+];
+
+function evaluar(nota){
+    return nota>6;
+}
+
+function examen(){
+    const aprobados=alumnos.filter(evaluar(alumnos.nota))
+    alert(aprobados)
+}
+examen()
